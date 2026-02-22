3e3d527f832dd042fef9ad1843fcfca39388366a
kai-bruell <kaibruell@proton.me>
2026-02-22 19:46:10 +0100

    feat: creating working nixos-test environment based on nixos-minimal

---
+++ terraform/environments/nixos-test/outputs.tf
@@ -0,0 +1,19 @@
+output "vm_id" {
+  description = "Proxmox VM ID"
+  value       = module.nixos_vm.vm_id
+}
+
+output "vm_name" {
+  description = "VM Name in Proxmox"
+  value       = module.nixos_vm.vm_name
+}
+
+output "bootstrap_ip" {
+  description = "Temporaere DHCP-IP (Bootstrap-Phase, danach statische IP aus Flake aktiv)"
+  value       = module.nixos_vm.bootstrap_ip
+}
+
+output "ssh_hint" {
+  description = "SSH-Verbindung nach Deploy (IP in nixos/hosts/nixos-test/default.nix einsehen)"
+  value       = module.nixos_vm.ssh_hint
+}
---
+++ terraform/environments/nixos-test/variables.tf
@@ -0,0 +1,99 @@
+# --- Proxmox Provider ---
+
+variable "proxmox_endpoint" {
+  description = "Proxmox API Endpoint (z.B. https://192.168.178.10:8006)"
+  type        = string
+}
+
+variable "proxmox_username" {
+  description = "Proxmox API Benutzer (z.B. root@pam)"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "proxmox_password" {
+  description = "Proxmox API Passwort"
+  type        = string
+  sensitive   = true
+}
+
+variable "proxmox_insecure" {
+  description = "TLS-Verifikation deaktivieren (nur fuer self-signed Zertifikate)"
+  type        = bool
+  default     = true
+}
+
+variable "proxmox_node" {
+  description = "Name des Proxmox Nodes (z.B. pve)"
+  type        = string
+}
+
+# --- VM ---
+
+variable "vm_id" {
+  description = "VM ID in Proxmox (muss eindeutig sein)"
+  type        = number
+}
+
+variable "bootstrap_template_id" {
+  description = "VM ID des Debian cloud-init Bootstrap-Templates (einmalig manuell erstellt)"
+  type        = number
+  default     = 9000
+}
+
+variable "snippet_datastore_id" {
+  description = "Proxmox Datastore fuer cloud-init Snippets (muss Snippet-Content aktiviert haben)"
+  type        = string
+  default     = "local"
+}
+
+variable "network_bridge" {
+  description = "Proxmox Network Bridge (z.B. vmbr0)"
+  type        = string
+  default     = "vmbr0"
+}
+
+variable "vm_name" {
+  description = "Name der VM in Proxmox"
+  type        = string
+  default     = "nixos-test"
+}
+
+variable "vcpu" {
+  description = "Anzahl virtueller CPUs"
+  type        = number
+  default     = 2
+}
+
+variable "sockets" {
+  description = "Anzahl CPU-Sockets"
+  type        = number
+  default     = 1
+}
+
+variable "memory" {
+  description = "RAM in MB"
+  type        = number
+  default     = 2048
+}
+
+# --- SSH ---
+
+variable "ssh_public_keys" {
+  description = "Liste von SSH Public Keys fuer Root-Zugang (cloud-init + NixOS authorized_keys)"
+  type        = list(string)
+}
+
+variable "ssh_private_key_path" {
+  description = "Pfad zum privaten SSH-Schluessel fuer nixos-anywhere"
+  type        = string
+  default     = "~/.ssh/homelab-dev"
+}
+
+# --- NixOS ---
+
+variable "nixos_flake_ref" {
+  description = "Flake-Referenz fuer nixos-anywhere. Format: <pfad>#<nixosConfigurations-attr>. Pfad relativ zum Verzeichnis von `tofu apply`."
+  type        = string
+  default     = "../../../nixos#nixos-test"
+}
